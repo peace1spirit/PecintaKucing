36ff62d314e798c22cfb24e411ad05940a268dd1
Rudy <peace1spirit@gmail.com>
2019-02-18 09:02:36 +0700

    add try catch

--- server/middleware/isLogin.js
+++ server/middleware/isLogin.js
@@ -8,17 +8,24 @@ const ObjectId = mongoose.Types.ObjectId;
 module.exports = {
     isLogin: (req, res, next) => {    
         if(req.headers.token){
-            let decoded = jwt.verify(req.headers.token, process.env.JWT);
-            let idCheck = ObjectId(decoded.id)
-            ModelUser.findOne({ _id : idCheck, email : decoded.email })
-            .then((result) => {
-                if(result) {
-                    req.currentuser=result
-                    next()
-                } else {
-                    res.status(403).json({ message : 'Token is not valid' });
-                }
-            })
+            try {
+                let decoded = jwt.verify(req.headers.token, process.env.JWT);
+                console.log(decoded)
+                let idCheck = ObjectId(decoded.id)
+                ModelUser.findOne({ _id : idCheck, email : decoded.email })
+                .then((result) => {
+                    if(result) {
+                        req.currentuser=result
+                        next()
+                    } else {
+                        res.status(403).json({ message : 'Token is not valid' });
+                    }
+                })
+            } catch (error) {
+                console.log(error)
+                res.status(403).json({ message : error.message }); 
+            }
+            
         }
         else
             res.status(403).json({ message : 'Token not found' });       
